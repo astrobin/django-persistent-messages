624d4d34fb745b96a30e69c02940efb651c84baf
Salvatore Iovene <salvatore@iovene.com>
2019-12-26 21:21:41 +0100

    Use callable views in urls, for Django 1.10 compatibility

--- persistent_messages/urls.py
+++ persistent_messages/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 
+from persistent_messages.views import message_detail, message_mark_all_read, message_mark_read
+
 urlpatterns = [
-    url(r'^detail/(?P<message_id>\d+)/$', 'persistent_messages.views.message_detail', name='message_detail'),
-    url(r'^mark_read/(?P<message_id>\d+)/$', 'persistent_messages.views.message_mark_read', name='message_mark_read'),
-    url(r'^mark_read/all/$', 'persistent_messages.views.message_mark_all_read', name='message_mark_all_read'),
+    url(r'^detail/(?P<message_id>\d+)/$', message_detail, name='message_detail'),
+    url(r'^mark_read/(?P<message_id>\d+)/$', message_mark_read, name='message_mark_read'),
+    url(r'^mark_read/all/$', message_mark_all_read, name='message_mark_all_read'),
 ]
